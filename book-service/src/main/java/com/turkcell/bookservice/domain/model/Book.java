2c71cd92f32761cd1f899345521b7dbea2cfd71f
1Mery <meryem.ozevren442001@gmail.com>
2025-11-02 10:45:31 +0300

    Accidentally made improvements to main

---
+++ book-service/src/main/java/com/turkcell/bookservice/domain/model/Book.java
@@ -0,0 +1,14 @@
+package com.turkcell.bookservice.domain.model;
+
+public class Book {
+    private final BookId id;
+    private String title;
+    private String author;
+    private Isbn isbn;
+    private int totalPage;
+    private String publisher;
+    private String imageUrl;
+
+
+}
+
